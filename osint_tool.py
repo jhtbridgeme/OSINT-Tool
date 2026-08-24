b666514cdf174f0f4de64df929672e7c23a74e63
jhtbridgeme <jhtrizwana@gmail.com>
2026-08-24 17:41:19 +0500

    Add basic OSINT tool for username searches

---
+++ osint_tool.py
@@ -0,0 +1,9 @@
+print("OSINT Tool")
+print("Welcome to my beginner OSINT tool")
+
+username = input("Enter a username: ")
+
+print("\nSearch links:")
+print("Google:", "https://www.google.com/search?q=" + username)
+print("GitHub:", "https://github.com/" + username)
+print("Reddit:", "https://www.reddit.com/search/?q=" + username)
